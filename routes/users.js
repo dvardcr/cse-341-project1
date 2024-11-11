3aceb315d75b5d34f5c1d82af99fb190cc051e3f
Diego Vargas <diego.vargasd@gmail.com>
2024-11-11 15:46:40 -0600

    Complete Step 1 Project1 - Add the PUT, POST, and DELETE routes

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,19 @@ const router = express.Router();
 
 const usersController = require('../controllers/users');
 
+//Get All
 router.get('/', usersController.getAll);
 
+//Get Single by Id
 router.get('/:id', usersController.getSingle);
+
+// Add new user
+router.post('/', usersController.createUser);
+
+// Modify user
+router.put('/:id', usersController.updateUser);
+
+// Remove user
+router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;
